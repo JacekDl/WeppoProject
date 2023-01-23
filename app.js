015618ae0a9adc3d83b090201131b33d1840211a
SzymonMleczko <smlyczek@gmail.com>
2023-01-23 19:19:20 +0100

    Test

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/login', user.getLogin);
+
+app.post('/login', ash(user.postLogin));
+
+app.get('/register', user.getRegister);
+
+app.post('/register', ash(user.postRegister));
+
+app.get('/logout', user.logout);
+
