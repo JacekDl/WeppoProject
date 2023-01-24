ddacc14647fa945722fd8331e18ebda9de1158a7
SzymonMleczko <smlyczek@gmail.com>
2023-01-24 13:59:38 +0100

    prosty działający server

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
-const express = require('express');
+var http = require('http');
+var express = require('express');
+var ejs = require('ejs');
+const path = require('path');
+//var todoRepo = require('./todos');
+const { application } = require('express');
+var app = express();
 
-const app = express();
+//var json = express.json();
 
-app.get('/login', user.getLogin);
+//app.disable('etag');
 
-app.post('/login', ash(user.postLogin));
 
-app.get('/register', user.getRegister);
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
-app.post('/register', ash(user.postRegister));
+app.use( express.static(path.join(__dirname,'public')))
 
-app.get('/logout', user.logout);
+app.use((req,res)=> {
+	res.render('index',{username:'admin'});
+});
+/*
+app.get( '/api/todo', (req, res) => {
+	res.json( todoRepo.getTodos() );
+});
 
+	app.post( '/api/todo', json, (req, res) => {
+	// middleware json od razu parsuje request
+	var todo = req.body;
+	var description = todo.description;
+	// dodawanie
+	var todo = todoRepo.addTodo(description);
+	// zwrócenie do klienta
+	res.json( todo );
+});
+app.put( '/api/todo/:id', json, (req, res) => {
+	// middleware json od razu parsuje request
+	var todo = req.body;
+	// powinna być walidacja!
+	// modyfikacja
+	var id = req.params.id;
+	todo = todoRepo.updateTodo(id, todo.description);
+	// zwrócenie do klienta
+	res.json(todo);
+})
+app.delete( '/api/todo/:id', (req, res) => {
+	// usuwanie
+	var id = req.params.id;
+	todoRepo.removeTodo(id);
+	res.status(200);
+	res.end();
+});
+app.get( '/', (req, res) => {
+	res.render( 'app' );	
+})
+*/
+http.createServer(app).listen(process.env.PORT || 3000);
+console.log( 'serwer działa' );
