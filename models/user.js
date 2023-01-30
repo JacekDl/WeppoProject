9809645271940a7f9ca41e660fe5bdc39d923176
jacek <ja.d@wp.pl>
2023-01-30 10:40:04 +0100

    Added password encryption and verification function

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
+var SALT_FACTOR = 10;
 
 var userSchema = mongoose.Schema({
     username: { type: String, required: true, unique: true },
@@ -9,6 +11,30 @@ var userSchema = mongoose.Schema({
 userSchema.methods.name = function() {
     return this.username;
 }
+
+var noop = function() {};
+userSchema.pre("save", function(done) {
+    var user = this;
+    if (!user.isModified("password")) {
+        return done();
+    }
+    bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+        if (err) { return done(err); }
+        bcrypt.hash(user.password, salt, noop, 
+            function(err, hashedPassword) {
+                if (err) { return done(err); }
+                user.password = hashedPassword;
+                done();
+            });
+        });
+});
+
+userSchema.methods.checkPassword = function(guess, done) {
+    bcrypt.compare(guess, this.password, 
+        function(err, isMatch) {
+            done(err, isMatch);
+        });
+};
 
 var User = mongoose.model("User", userSchema);
 
