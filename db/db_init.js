3d70cd7925fdab539008dd48cadcc52c8e8198e1
jacek <ja.d@wp.pl>
2023-01-31 22:11:45 +0100

    Changed connection to db from local to cloud

--- db/db_init.js
+++ db/db_init.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 const User = require('../models/user');
 
-mongoose.connect("mongodb://127.0.0.1:27017/product");
+// mongoose.connect("mongodb://127.0.0.1:27017/product");
+mongoose.connect("mongodb+srv://weppo:VfJ8CpO55Oj8QFwp@cluster0.xrcaeau.mongodb.net/?retryWrites=true&w=majority");
 
 run()
 async function run() {
