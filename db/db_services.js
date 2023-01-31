d0cef05e43b512d28d026c8676d4c33c8914e774
SzymonMleczko <smlyczek@gmail.com>
2023-01-31 21:44:25 +0100

    confirmation of db iterface

--- db/db_services.js
+++ db/db_services.js
@@ -1,3 +1,4 @@
+
 async function login_user(username,password){
 	if(username == password){
 		if(username == 'admin' || username == 'sauron'||username == 'saruman')
@@ -8,8 +9,24 @@ async function login_user(username,password){
 	} else {
 		return null;
 	}
-}
+}//może być po prostu jason ze wszystkim
+async function add_user(){}
+
+async function give_all_product(){}
+async function find_by_name(name){}
+async function give_all_users(){}
+async function add_product(name,description,price){}
+async function delete_product(name){}
+async function update_product(name, description,price){}
+
+async function give_all_orders(){}
+async function add_order(name,date,order,closed){} //order:jason{list[product]},closed:Boolean
+async function update_order(id_order,closed){}
+
 
 module.exports = {
 	login_user
 }
+//index - cokie - jak przekazać , liste produktów jakoś
+//basket - cokie - pamiętanmie listy wybranych produktów ( obiekt js ) 
+//lista 
